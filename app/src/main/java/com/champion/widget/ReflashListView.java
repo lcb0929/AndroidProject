07198ba90997c906b9b736ca5ef309a115124d40
ChampionLam <gd_lcb0929@163.com>
2015-09-17 20:47:09 +0800

    Add RefreshListView Demo

---
+++ app/src/main/java/com/champion/widget/ReflashListView.java
@@ -0,0 +1,16 @@
+package com.champion.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by ChampionLam on 2015/9/17.
+ */
+public class ReflashListView extends ListView {
+
+
+    public ReflashListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
